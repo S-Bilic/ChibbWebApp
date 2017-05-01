ffab4ab3ecbb8f0d7ec10d7e9e91da4c23e2d6e4
stefanbilic <stefanbiliccc@gmail.com>
2017-05-01 21:50:22 +0200

    Initial commit

---
+++ public/js/login.js
@@ -0,0 +1,3 @@
+$(".btn-register").click(function(){
+    location.href='http://localhost:3000/register';
+});
---
+++ public/js/register.js
@@ -0,0 +1,3 @@
+$(".btn-register-back").click(function(){
+    location.href='http://localhost:3000/login';
+});
