b9866d0a01145ee5ec15b0d50d8c59aaad04f1be
stefanbilic <stefanbiliccc@gmail.com>
2017-05-22 00:51:58 +0200

    Added styling in dashboard and update functions for the charts.

--- public/js/line_chart.js
+++ public/js/line_chart.js
@@ -13,10 +13,11 @@ function processRequest(e) {
         var temp = [];
         var sortedDate = [];
         var unsortedDate = [];
+        var sortedTime = [];
 
         data.forEach(function (sensorNode) {
-           temp.push(sensorNode.reading);
-           unsortedDate.push(sensorNode.timestamp);
+            temp.push(sensorNode.reading);
+            unsortedDate.push(sensorNode.timestamp);
         });
 
         var sort = unsortedDate.sort(function(x, y){
@@ -28,41 +29,70 @@ function processRequest(e) {
             console.log(date);
             formattedDate = date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear();
             sortedDate.push(formattedDate);
-            // formattedTime = date.getHours() + ';' + date.getMinutes() + ';' + date.getSeconds();
-            // sortedDate.push(formattedTime);
+            formattedTime = date.getHours() + ';' + date.getMinutes() + ';' + date.getSeconds();
+            sortedTime.push(formattedTime);
         });
+
+
 
         // Use the result array in the chart data
         var ctx = document.getElementById("myChart4");
         var myChart4 = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: sortedDate,
+                // labels: sortedDate,
+                labels: ['day 1', 'day 2', 'day 3', 'day 4'],
                 datasets: [{
                     label: 'Temperature',
-                    data: temp,
+                    // data: temp,
+                    data: [1,5,10,13,12],
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
                     borderColor: 'rgba(255,99,132,1)',
                     borderWidth: 1
                 }]
             },
             options: {
+                responsiveAnimationDuration: 3500,
                 scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            maxTicksLimit: 20
+                        }
+                    }],
                     yAxes: [{
                         ticks: {
-                            beginAtZero:true
+                            beginAtZero:true,
+                            stepSize : 5
                             // maxTicksLimit: 20
                         }
                     }]
-                    // xAxes: [{
-                    //     ticks: {
-                    //         beginAtZero:true,
-                    //         maxTicksLimit: 5
-                    //     }
-                    // }]
                 }
             }
         });
+
+        function updateData() {
+            myChart4.data.datasets[0].data = temp;
+            myChart4.data.labels = sortedDate;
+            myChart4.update();
+        };
+
+        document.getElementById("updateData").onclick = updateData;
+
+        function SwitchTimeFormat () {
+            myChart4.data.labels = sortedTime;
+            myChart4.update();
+        }
+
+        document.getElementById("SwitchTimeFormat").onclick = SwitchTimeFormat;
+
+        function SwitchDateFormat () {
+            myChart4.data.labels = sortedDate;
+            myChart4.update();
+        }
+
+        document.getElementById("SwitchDateFormat").onclick = SwitchDateFormat;
+
 
         var ctx = document.getElementById("myChart3");
         var myChart3 = new Chart(ctx, {
@@ -80,6 +110,7 @@ function processRequest(e) {
                 }]
             },
             options: {
+                responsiveAnimationDuration: 3500,
                 scales: {
                     yAxes: [{
                         ticks: {
@@ -89,64 +120,71 @@ function processRequest(e) {
                 }
             }
         });
-
-        var pieData = {
-            labels: [
-                "Purple",
-                "Green",
-                "Orange",
-                "Yellow",
-            ],
-            datasets: [
-                {
-                    data: temp,
-                    backgroundColor: [
-                        "#878BB6",
-                        "#4ACAB4",
-                        "#FF8153",
-                        "#FFEA88",
-                        "#878BB6",
-                        "#4ACAB4",
-                        "#FF8153",
-                        "#FFEA88",
-                        "#878BB6",
-                        "#4ACAB4",
-                        "#FF8153",
-                        "#FFEA88"
-                    ]
-                }]
-        };
-
-        var ctx = document.getElementById("myChart2").getContext("2d");
-        var myChart2 = new Chart(ctx, {
-            type: 'doughnut',
-            data: pieData
-        });
-
-        var ctx = document.getElementById("myChart1");
-        var myChart1 = new Chart(ctx, {
-            type: 'radar',
-            data: {
-                // labels:['Red', 'Blue', 'Green', 'Yellow'],
-                labels: sortedDate,
-                datasets: [{
-                    label: 'Temperature',
-                    // data:[5, 10, 15, 40],
-                    data: temp,
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255,99,132,1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
-            }
-        });
+        //
+        // var pieData = {
+        //     labels: [
+        //         "Purple",
+        //         "Green",
+        //         "Orange",
+        //         "Yellow",
+        //     ],
+        //     datasets: [
+        //         {
+        //             data: temp,
+        //             backgroundColor: [
+        //                 "#878BB6",
+        //                 "#4ACAB4",
+        //                 "#FF8153",
+        //                 "#FFEA88",
+        //                 "#878BB6",
+        //                 "#4ACAB4",
+        //                 "#FF8153",
+        //                 "#FFEA88",
+        //                 "#878BB6",
+        //                 "#4ACAB4",
+        //                 "#FF8153",
+        //                 "#FFEA88"
+        //             ]
+        //         }
+        //     ]
+        // };
+        //
+        // var ctx = document.getElementById("myChart2").getContext("2d");
+        // var myChart2 = new Chart(ctx, {
+        //     type: 'polarArea',
+        //     data: pieData,
+        //     options: {
+        //         animation:{
+        //             animateScale:true,
+        //             responsiveAnimationDuration: 3500,
+        //         }
+        //     }
+        // });
+        //
+        // var ctx = document.getElementById("myChart1");
+        // var myChart1 = new Chart(ctx, {
+        //     type: 'pie',
+        //     data: {
+        //         labels: sortedDate,
+        //         datasets: [{
+        //             label: 'Temperature',
+        //             // data:[5, 10, 15, 40],
+        //             data: temp,
+        //             backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        //             borderColor: 'rgba(255,99,132,1)',
+        //             borderWidth: 1
+        //         }]
+        //     },
+        //     options: {
+        //         responsiveAnimationDuration: 3500,
+        //         scales: {
+        //             yAxes: [{
+        //                 ticks: {
+        //                     beginAtZero:true
+        //                 }
+        //             }]
+        //         }
+        //     }
+        // });
     }
 }
