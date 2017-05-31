2acd99f3cc213577c027679e21be3f59e8222840
stefanbilic <stefanbiliccc@gmail.com>
2017-06-01 01:13:57 +0200

    Fixed Iframe isues when creating new charts. Added amount input function. Updated styling,

--- public/js/line_bar_chart.js
+++ public/js/line_bar_chart.js
@@ -16,21 +16,29 @@ function line_bar_chart(link, chartType, id, color, bordercolor, label, Ylabel) 
 
             var response = JSON.parse(xhr.responseText);
             var data = response;
-            // reverse();
             var reading = [];
-            // var sortedDate = [];
-            var unsortedDate = [];
-            // var sortedTime = [];
+            var date = [];
+            var dateDays = [];
+            var dateTime = [];
 
             data.forEach(function (sensorNode) {
                 reading.push(sensorNode.reading);
-                unsortedDate.push(sensorNode.date);
+                date.push(sensorNode.date);
             });
 
-            // recentY = reading.slice(Math.max(readingLength -5));
             recentY = reading;
-            recentDateX = unsortedDate;
 
+
+            date.forEach(function (sensorDate) {
+                splitDate = sensorDate.split(" ");
+                day = splitDate[0];
+                dateDays.push(day);
+                time = splitDate[1];
+                dateTime.push(time);
+            });
+
+            recentDateX = dateDays;
+            recentTimeX = dateTime;
 
             // var format = unsortedDate;
             //
@@ -57,11 +65,24 @@ function line_bar_chart(link, chartType, id, color, bordercolor, label, Ylabel) 
             // recentTimeX = sortedTime;
 
             // Use the result array in the chart data
+
+
+            if (id == "myChart") {
+                $('#myChart').remove();
+                $('iframe.chartjs-hidden-iframe').remove();
+                $('#graphContainer').append('<canvas id="myChart"><canvas>');
+            }
+            if (id == "myChart1") {
+                $('#myChart1').remove();
+                $('iframe.chartjs-hidden-iframe').remove();
+                $('#graphContainer1').append('<canvas id="myChart1"><canvas>');
+            }
+
             var ctx = document.getElementById(id);
             var myChart = new Chart(ctx, {
                 type: chartType,
                 data: {
-                    labels: recentDateX,
+                    labels: recentTimeX,
                     // labels: ['day 1', 'day 2', 'day 3', 'day 4'],
                     datasets: [{
                         label: label,
@@ -70,16 +91,7 @@ function line_bar_chart(link, chartType, id, color, bordercolor, label, Ylabel) 
                         backgroundColor: color,
                         borderColor: bordercolor,
                         borderWidth: 1
-                    },
-                        {
-                            type:chartType,
-                            label: label,
-                            // data: recentY,
-                            data: [1,5,10,13,12],
-                            backgroundColor: color,
-                            borderColor: bordercolor,
-                            borderWidth: 1
-                        }
+                    }
                     ]
                 },
                 options: {
@@ -106,36 +118,69 @@ function line_bar_chart(link, chartType, id, color, bordercolor, label, Ylabel) 
                 }
             });
 
-            $(document).ready(function () {
-                $('#chartLink, #chartLink2').change(function () {
-                    var Temp = "temperature";
-                    var Humid = "humidity";
-                    var urlAddress = "http://145.24.222.154/api/";
-                    var urlQuery = "?limit=5&sort=-date&sensor_id=";
 
-                    $("#inputTemp1").val(urlAddress + Temp + urlQuery + 1);
-                    $("#inputTemp2").val(urlAddress + Temp + urlQuery + 2);
-                    $("#inputTemp3").val(urlAddress + Temp + urlQuery + 3);
+            // $(document).ready(function () {
+            //     var obj = new Object();
+            //     $('#chartLink').change(function () {
+            //         input = chartLink = obj.chartLink = $('#chartLink').val();
+            //
+            //         if (input) {
+            //             myChart.clear();
+            //             createChart(chartLink, "line", "myChart", "rgba(255, 99, 132, 0.2)", "rgb(255, 99, 132)",  'Temperature', '°C');
+            //         }
+            //         else {
+            //             alert('error');
+            //         }
+            //     });
+            // });
 
-                    $("#inputHumid1").val(urlAddress + Humid + urlQuery + 1);
-                    $("#inputHumid2").val(urlAddress + Humid + urlQuery + 2);
-                    $("#inputHumid3").val(urlAddress + Humid + urlQuery + 3);
-
-                    var obj = new Object();
-                    input =
-                        chartLink = obj.chartLink = $('#chartLink, #chartLink2').val();
-
-                    if (input) {
-                        createChart(chartLink, "line", "myChart", "rgba(255, 99, 132, 0.2)", "rgb(255, 99, 132)",  'Temperature', '°C');
-                        if (id == 'myChart') {
-                            myChart.destroy();
-                        }
-                    }
-                    else {
-                        alert('error');
-                    }
-                });
-            });
+            // $(document).ready(function () {
+            //
+            //     var Temp = "temperature";
+            //     var Humid = "humidity";
+            //     var urlAddress = "http://145.24.222.154/api/";
+            //     var urlQuery = "?limit=5&sort=-date&sensor_id=";
+            //     var obj = new Object();
+            //
+            //     $('#chartLink').change(function () {
+            //
+            //         // $("#inputTemp1").val(urlAddress + Temp + urlQuery + 1);
+            //         // $("#inputTemp2").val(urlAddress + Temp + urlQuery + 2);
+            //         // $("#inputTemp3").val(urlAddress + Temp + urlQuery + 3);
+            //
+            //         input = chartLink = obj.chartLink = $('#chartLink').val();
+            //
+            //
+            //         if (input) {
+            //             createChart(chartLink, "line", "myChart", "rgba(255, 99, 132, 0.2)", "rgb(255, 99, 132)",  'Temperature', '°C');
+            //             if (id == "myChart") {
+            //                 myChart.destroy();
+            //             }
+            //         }
+            //         else {
+            //             alert('error');
+            //         }
+            //     });
+            //
+            //     $('#chartLink2').change(function () {
+            //
+            //         $("#inputHumid1").val(urlAddress + Humid + urlQuery + 1);
+            //         $("#inputHumid2").val(urlAddress + Humid + urlQuery + 2);
+            //         $("#inputHumid3").val(urlAddress + Humid + urlQuery + 3);
+            //
+            //         input2 = chartLink2 = obj.chartLink2 = $('#chartLink2').val();
+            //
+            //         if (input2) {
+            //             createChart(chartLink2, "bar", "myChart1", "rgba(180, 226, 243, 0.5)", "rgb(150, 226, 243)", 'Humidity', '%');
+            //             if (id == 'myChart1') {
+            //                 myChart.destroy();
+            //             }
+            //         }
+            //         else {
+            //             alert('error');
+            //         }
+            //     });
+            // });
 
             // function updateData() {
             //     myChart.data.datasets[0].data = recentY;
@@ -152,9 +197,9 @@ function line_bar_chart(link, chartType, id, color, bordercolor, label, Ylabel) 
 
             var astate = 0;
 
-            $('.switchFormat').click(function () {
+            $('#switchFormat').click(function (e) {
                 // $(this).click("click", function () {
-                    if (astate == 0) {
+                    if (astate == 0 && id == "myChart") {
                         myChart.data.labels = recentTimeX;
                         myChart.update();
                         astate = 1;
@@ -163,6 +208,19 @@ function line_bar_chart(link, chartType, id, color, bordercolor, label, Ylabel) 
                         myChart.update();
                         astate = 0;
                     }
+                // });
+            });
+            $('#switchFormat1').click(function (e) {
+                // $(this).click("click", function () {
+                if (astate == 0 && id == "myChart1") {
+                    myChart.data.labels = recentTimeX;
+                    myChart.update();
+                    astate = 1;
+                } else {
+                    myChart.data.labels = recentDateX;
+                    myChart.update();
+                    astate = 0;
+                }
                 // });
             });
 
@@ -261,3 +319,4 @@ function line_bar_chart(link, chartType, id, color, bordercolor, label, Ylabel) 
         }
     }
 };
+
