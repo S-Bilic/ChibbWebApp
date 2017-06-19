43e16e4d57198abdf3b70ee81131f04f25d7e8a3
stefanbilic <stefanbiliccc@gmail.com>
2017-06-19 12:39:53 +0200

    test run 7

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var request = require("request");
 // var base_url2 = "http://145.24.222.154/api/humidity";
-var base_url = "http://145.24.222.154/api/temperature";
+var base_url = "http://145.24.222.154/api/temperature?limit=5";
 
 describe("GET response from api link /", function() {
     it("returns status code 200", function(done) {
