430f5a304d2dbfd67fcbee9c832c9ff530764552
stefanbilic <stefanbiliccc@gmail.com>
2017-06-20 16:03:55 +0200

    test run 5

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var request = require("request");
-// var base_url2 = "http://145.24.222.154/api/humidity"
+// var base_url2 = "http://145.24.222.154/api/humidity";
 var base_url = "http://145.24.222.154/api/temperature?limit=5";
 
 describe("GET response from api link /", function() {
